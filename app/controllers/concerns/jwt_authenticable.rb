0bd6522bd380f0de8d5566ca9b165afb5628cfcf
MarianoGuastella <marianoguastella@hotmail.com>
2024-12-16 16:59:07 -0300

    chore: remove more pointless logs

--- app/controllers/concerns/jwt_authenticable.rb
+++ app/controllers/concerns/jwt_authenticable.rb
@@ -5,9 +5,8 @@ module JwtAuthenticable
 
   def authenticate_user
     header = request.headers["Authorization"]
-    Rails.logger.info "Authenticating request with header: #{header.present? ? 'present' : 'missing'}"
-
     token = header.split(" ").last if header
+
     begin
       decoded = JWT.decode(token, Rails.application.secret_key_base, true, algorithm: "HS256")
       @current_user = User.find(decoded[0]["user_id"])
